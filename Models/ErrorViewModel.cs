d6c53477fdf165c2e16523ec7fdbccc805afa897
Anastasios Moraitis <anas.moraitis@gmail.com>
2018-11-06 01:50:51 +0200

    Add final project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bootstrap4Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
